6e4802b60878a4e966401a08ead4f272b56c7eb8
Ezequiel Korenblum <ezequiel@losko.com.ar>
2021-09-29 20:07:40 -0300

    Agregados nuevos tios de indicadores

--- src/indicatorsGraph.tsx
+++ src/indicatorsGraph.tsx
@@ -4,32 +4,65 @@ import store from "./apis/store";
 
 export default function Graph(props: any) {
     // if (props.id == "placeholder") debugger;
-    const data = store.getState().indicators.indicators.find((obj) => {
+    const data = store.getState().indicators.indicators.find((obj: any) => {
         return obj.id == props.id;
     });
-    console.log(props.id, data);
 
-    return (
-        <Plot
-            data={
-                data !== undefined
-                    ? data.value.data
-                    : [
-                          {
-                              x: [1, 2, 3],
-                              y: [2, 6, 3],
-                              type: "scatter",
-                              mode: "lines+markers",
-                              marker: { color: "red" },
-                          },
-                          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
-                      ]
-            }
+    if (data.title == "testo") console.log(data)
+    let plot;
+    if (data?.value.data) {
+        plot = <Plot
+            data={ data.value.data }
             layout={{
                 width: 320,
                 height: 240,
                 title: data !== undefined ? data.title : "Placeholder",
             }}
         />
-    );
+    } else if (data?.type == "progress") {
+        plot = <Plot
+            data={[{
+                value: data.value,
+                type: "indicator",
+                number: { suffix: "%" },
+                mode: "gauge+number",
+                gauge: { axis: { range: [null, 100], visible: false }}
+            }]}
+            layout={{
+                width: 320,
+                height: 240,
+                title: data !== undefined ? data.title : "Placeholder",
+            }}
+        />
+    } else if (data?.type == "number" || data?.type == "counter") {
+        plot = <Plot
+            data={[{
+                type: "indicator",
+                mode: "number",
+                value: data.value,
+            }]}
+            layout={{
+                width: 320,
+                height: 240,
+                title: data !== undefined ? data.title : "Placeholder",
+            }}
+        />
+    } else if (data?.type == "text") { // WIP: Falta el diseño
+        plot = <div className={ data.type } style={{
+            position: 'relative',
+            display: 'inline-block',
+            width: 320, 
+            height: 240,
+            fontSize: '12px'
+        }}>
+            <div className="title" style={{
+                fontSize: '1.2em',
+                textAlign: 'center'
+            }}>{ data.title }</div>
+            <div className="value">{ data.value }</div>
+        </div>
+    } else {
+        plot = <div className="none"></div>
+    }
+    return plot
 }
